3a6d1e170d3510c3fdeb9cb5b6bf5290524301d2
TrianaSolerMartin <triana287@gmail.com>
2024-03-04 09:18:30 +0100

    using method

--- POO/main.js
+++ POO/main.js
@@ -12,5 +12,14 @@ class Rectangulo {
 const miRectangulo = new Rectangulo(12, 33);
 const miRectangulopequeño = new Rectangulo(8, 10);
 
+//reutilizar clases
 console.log(miRectangulo);
 console.log(miRectangulopequeño);
+
+//reutilizar metodo
+console.log(miRectangulo.obtenerInfo());
+console.log(miRectangulopequeño.obtenerInfo())
+
+//reutilizar parametros
+
+console.log(miRectangulo.ancho);
